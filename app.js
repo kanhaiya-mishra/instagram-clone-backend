6fd6831132698885987dae7f75726f61b0756d41
Kanhaiya Mishra <kanhaiya.mishra@comprotechnologes.com>
2021-01-24 16:56:38 +0530

    updating config path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const config = require('./config/dev');
+const config = require('./config');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
